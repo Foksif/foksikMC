fa29accaab5fe5422635e91368b0831cb3e4b320
Foksif <knenrerp@gmail.com>
2026-06-06 15:17:48 +0700

    paper version

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -16,7 +16,7 @@ publishing {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
